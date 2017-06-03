c9ad4b58314ba995d10bfc07109086a72eb3c444
Dianne <dianneprinsescah@gmail.com>
2017-06-03 08:13:26 +0300

    Javascript 1

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 'use strict'
+
+// inheritance
 var Animal = function(name,color,legs) {
     this.name = name;
     this.color=color;
@@ -6,20 +8,15 @@ var Animal = function(name,color,legs) {
 }
 
 var animal1 = new Animal("Gazelle" , "Brown", 4);
-
 var animal2 = new Animal('Cow' ,'Black', 4);
-
 var animal3 = new Animal('Pig' ,'pink', 4);
-
 var animal4 = new Animal('Rabbit' ,'white', 4);
 
 
 document.getElementById('gazelle').onclick = function()
 {
 	document.getElementById('p1').innerHTML="The first animal is: " +animal1.name+ ", "+ "color: "+ animal1.color+ ", "+"Number of legs:"+animal1.legs;
-	
 }
-
 
 document.getElementById('cow').onclick = function()
 {
@@ -33,5 +30,21 @@ document.getElementById('pig').onclick = function()
 
 document.getElementById('rabbit').onclick = function()
 {
- document.getElementById('p4').innerHTML="The fourth animal is: " +animal4.name+ ", "+ "color: "+ animal4.color+ ", "+"Number of legs:"+animal4.legs;
- }
+ 	document.getElementById('p4').innerHTML="The fourth animal is: " +animal4.name+ ", "+ "color: "+ animal4.color+ ", "+"Number of legs:"+animal4.legs;
+}
+
+
+// closure example
+document.getElementById('closure').onclick = function(){
+	var myFunc = getName();
+	myFunc();
+}
+function getName() {
+  var name = prompt('Enter your Name');
+  function displayName() {
+    alert('Hello '+name+', Welcome to this portal');
+    document.getElementById('p5').innerHTML="Name: " +name;
+  }
+  return displayName;
+}
+
